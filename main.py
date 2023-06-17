1cd61363636f0b3877d1fe36122e09e77c714f71
Maxwell Rutherford <javert.hugo@yandex.ru>
2023-06-17 16:10:53 +0300

    add comments

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ import tkinter.scrolledtext as scrolledtext
 def create_sequence_from_double_nested_array(double_nested_array):
     return ''.join([''.join([str(char) for char in nested_array]) for nested_array in deepcopy(double_nested_array)])
 
-# сделать вектор необходимой длины
+# сделать вектор необходимой длины, просто добавляем 0 в конец списка,
+# пока список не станет необходимой длины
 def make_vector_need_len(vector, vector_length):
     vector_copy = deepcopy(vector)
     while len(vector_copy) < vector_length:
@@ -35,29 +36,31 @@ def get_vector_from_power(power):
     vector[power] = 1
     return vector
 
-# получаем кол-во ненулевых элементов в векторе
+# получаем кол-во ненулевых элементов (единиц) в векторе
 def get_wt(vector):
     return sum(vector)
 
 # получаем синдром из вектора (кодового слова)
 def get_syndrome_from_vector(code_word, gen_polynom, n):
+    # синдром - остаток от деления кодового слова на порождающий полином
     syndrome = list(Polynomial(code_word) % Polynomial(gen_polynom))
-    for i in range(len(syndrome)):
-        if syndrome[i] % 2 == 0:
-            syndrome[i] = 0
-        else:
-            syndrome[i] = 1
-    while len(syndrome) < n:
-        syndrome.append(0)
+
+    # делаем полученный вектор биноминальным
+    syndrome = get_binom_vector(syndrome)
+
+    # делаем вектор необходимой длины, путём добавления в конец нулей
+    syndrome = make_vector_need_len(syndrome, n)
+
     return syndrome
 
 # получаем длину кодовых слов (m + k - сумма макс степени порожд полинома и длины инф слов)
 def get_n(g, len_of_inf_word):
     gen_polynom = deepcopy(list(g))
     m = 0
-    for i in range(len(g),0,-1):
-        if gen_polynom[i-1] == 1:
-            m = i-1
+    # ищем индекс первого ненулевого элемента массива, начиная с конца
+    for i in range(len(g)-1,0,-1):
+        if gen_polynom[i] == 1:
+            m = i
             break
     return m + len_of_inf_word
 
@@ -66,38 +69,47 @@ def get_n(g, len_of_inf_word):
 
 # получаем информационные слова заданной длины из переданного текста
 def get_inf_words(text, len_of_inf_word, need_len_to_make_char_from_inf_word):
-    inf_words = [bin(ord(char))[2:] for char in list(text)]
+    inf_words = [bin(ord(char))[2:] for char in list(text)] # преобразуем текст в массив по типу: ['110001', '110010', '110011', '1010']
     for i in range(len(inf_words)):
-        inf_words[i] = list(inf_words[i])
-        inf_words[i] = [int(num) for num in inf_words[i]]
-        inf_words[i].reverse()
+        inf_words[i] = list(inf_words[i]) # преобразуем каждый элемент массива в список: '110001' -> ['1', '1', '0', '0', '0', '1']
+        inf_words[i] = [int(num) for num in inf_words[i]] # преобразуем каждый элемент массива в список чисел: [1, 1, 0, 0, 0, 1]
+        inf_words[i].reverse() # разворачиваем каждый элемент массива: [1, 1, 0, 0, 0, 1] -> [1, 0, 0, 0, 1, 1]
+
+        # делаем каждый элемент массива необходимой длины: [1, 0, 0, 0, 1, 1] -> [1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
         inf_words[i] = make_vector_need_len(inf_words[i], need_len_to_make_char_from_inf_word)
+
+        # преобразуем каждый элемент массива в строку: [1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0] -> 110001000000
         inf_words[i] = ''.join([str(num) for num in inf_words[i]])
+    
+    # преобразовать весь массив инф слов в одну последовательность цифр
     inf_words = [int(char) for char in list(''.join(inf_words))]
+
+    # считаем количество информационных слов необходимой длины, а именно 7
     count_of_inf_word_need_len = ceil(len(inf_words) // len_of_inf_word) + 1
+
+    # добавляем в конец последовательности необходимое число нули, чтобы потом разделить эту последователность на
+    # информационные слова одинаковой длины
     inf_words = make_vector_need_len(inf_words, len_of_inf_word * count_of_inf_word_need_len)
     inf_words_need_len = []
+
+    # делим эту последователность на информационные слова одинаковой длины
     for j in range(count_of_inf_word_need_len): inf_words_need_len.append(inf_words[j*len_of_inf_word:(j+1)*len_of_inf_word])
     
-    # print(len_of_inf_word * ceil(len(inf_words) // len_of_inf_word))
-    # print(len(inf_words))
-    # print(inf_words_need_len)
     return inf_words_need_len
 
 # получаем кодовое слово из информационного слова
 def get_code_words(inf_words, gen_polynom, n):
     code_words = []
     for inf_word in inf_words:
-        # print(inf_word)
-        # print(gen_polynom)
+        # получаем кодовое слово из информационного слова путём произведения
+        # информационного слова на порождающий полином
         code_word = list(Polynomial(inf_word) * Polynomial(gen_polynom))
-        for i in range(len(code_word)):
-            if code_word[i] % 2 == 0:
-                code_word[i] = 0
-            else:
-                code_word[i] = 1
-        while len(code_word) < n:
-            code_word.append(0)
+
+        # делаем полученный вектор биноминальным
+        code_word = get_binom_vector(code_word)
+
+        # делаем вектор необходимой длины, путём добавления в конец нулей
+        code_word = make_vector_need_len(code_word, n)
         code_words.append(code_word)
     return code_words
 
@@ -107,20 +119,20 @@ def make_mistake_in_vector(vector, num_of_errors):
     # # Если хотим чтобы ошибки не накладывались друг на друга - раскоментировать нижнее
     num_memory = []
     # #
-    # print(vector_copy)
     for i in range(randint(0,num_of_errors)):
+        # получаем случайный индекс, где необходимо допустить ошибку
         num = randint(0, len(vector_copy) - 1)
         # # Если хотим чтобы ошибки не накладывались друг на друга - раскоментировать нижнее
+        # если уже был использован, генерируем другой
         while num in num_memory:
             num = randint(0, len(vector_copy) - 1)
         num_memory.append(num)
-        # # 
+        
+        # меняем значение бита на противоположное значение
         if (vector_copy[num] == 0):
             vector_copy[num] = 1
         else:
             vector_copy[num] = 0
-    # print(vector_copy)
-    # print('#####################')
     return vector_copy
 
 # делаем произвольное число ошибок (от 0 до num_of_errors) ошибок во всех векторах
@@ -132,31 +144,38 @@ def make_mistake_in_vectors(vectors, num_of_errors):
 
 # исправляем ошибки в кодовых словах
 def correct_mistake_in_code_word(code_word_with_mistakes, gen_polynom, n, t):
+    # вычисляем синдром
     initial_syndrome = get_syndrome_from_vector(code_word_with_mistakes, gen_polynom, n)
     # print('initial_syndrome: ', initial_syndrome)
 
     initial_code_word = []
     error_vector = []
 
-    # если в кодовом слове нет ошибки
+    # если в кодовом слове нет ошибки, возвращаем полученное кодовое слово
     if (sum(initial_syndrome) == 0):
         initial_code_word = code_word_with_mistakes
         # print('1) initial_code_word: ', initial_code_word)
- 
+
+    # если в синдроме число ненулевых элементов (единиц) находится в пределе от 0 до t,
+    # где t - число гарантированно исправляемых ошибок кодом,
+    # то считаем данный синдром вектором e(x)
+    # и возвращаем v(x)-e(x)
     elif (get_wt(initial_syndrome) <= t):
         error_vector = initial_syndrome
         initial_code_word = get_binom_vector([int(num) for num in list(Polynomial(code_word_with_mistakes) - Polynomial(error_vector))])
         initial_code_word = make_vector_need_len(initial_code_word, n)
         # print('2) initial_code_word: ', initial_code_word)
 
+    # если в синдроме число ненулевых элементов (единиц) больше t
     else:
         for i in range(1,n):
+            # вычисляем "текущий" синдром по следующей формуле: x*s(i-1) mod(g)
             current_syndrome = get_binom_vector(list((Polynomial([0, 1]) * Polynomial(initial_syndrome)) % Polynomial(gen_polynom)))
             # print('initial_syndrome: ', initial_syndrome)
             # print('current_syndrome: ', current_syndrome)
             initial_syndrome = current_syndrome
-            # while len(current_syndrome) < n:
-            #     current_syndrome.append(0)
+            # если в полученном синдроме число ненулевых элементов (единиц) находится в пределе от 0 до t,
+            # то находим вектор ошибок по формуле: x^(n-i) * s(i) mod(x^n + 1)
             if (get_wt(current_syndrome) <= t):
                 # print(n)
                 # print(list(Polynomial(get_vector_from_power(n)) + 1))
@@ -164,19 +183,17 @@ def correct_mistake_in_code_word(code_word_with_mistakes, gen_polynom, n, t):
                 error_vector1 = list((Polynomial(get_vector_from_power(n-i)) * Polynomial(current_syndrome)) % (Polynomial(get_vector_from_power(n)) + 1))
                 error_vector1 = get_binom_vector(error_vector1)
                 # print('error_vector1: ', error_vector1)
-                # while len(error_vector) < n:
-                #     error_vector.append(0)
                 initial_code_word = get_binom_vector(list(Polynomial(code_word_with_mistakes) - Polynomial(error_vector1)))
                 initial_code_word = make_vector_need_len(initial_code_word, n)
-                # print('cd_wd: ', initial_code_word)
-                initial_syndrome = current_syndrome
+                # initial_syndrome = current_syndrome
 
+                # print('3) initial_code_word: ', initial_code_word)
                 break
-                # break
             else:
                 initial_syndrome = current_syndrome
     return initial_code_word
 
+# исправляем ошибки во всех векторах
 def correct_mistake_in_code_words(code_words_with_mistakes, gen_polynom, n, t):
     correct_code_words = []
     for code_word_with_mistake in code_words_with_mistakes:
@@ -184,14 +201,10 @@ def correct_mistake_in_code_words(code_words_with_mistakes, gen_polynom, n, t):
     return correct_code_words
 
 
-# получаем информационное слово из кодового 
+# получаем информационное слово из кодового путём целочисленного деления кодового слова на порождающий полином
 def get_inf_word_from_code_word(code_word, gen_polynom, len_of_inf_word):
     inf_word = list(Polynomial(code_word) // Polynomial(gen_polynom))
-    for i in range(len(inf_word)):
-        if inf_word[i] % 2 == 0:
-            inf_word[i] = 0
-        else:
-            inf_word[i] = 1
+    inf_word = get_binom_vector(inf_word)
     inf_word = make_vector_need_len(inf_word, len_of_inf_word)
     return inf_word
 
@@ -202,17 +215,17 @@ def get_inf_words_from_code_words(code_words, gen_polynom, len_of_inf_word):
         decoded_inf_words.append(get_inf_word_from_code_word(code_word, gen_polynom, len_of_inf_word))
     return decoded_inf_words
 
-# получить букву из информационного слова
-# def make_char_from_inf_word(inf_word):
-#     return chr(int('0b' + ''.join([str(num) for num in reversed(deepcopy(inf_word))]), 2))
-
-# получить буквы из информационных слов
+# получаем символы (буквы) из информационных слов
 def make_char_from_inf_words(inf_words, need_len_to_make_char_from_inf_word):
     decoded_text = ''
     text = deepcopy(inf_words)
+    # преобразовываем информационные слова в последовательность информационных слов
     for i in range(len(text)):
         text[i] = ''.join([str(char) for char in text[i]])
     text = ''.join(text)
+
+    # получаем символ из каждого куска последовательности информационных слов
+    # и добавляем данный символ к переменной decoded_text
     for j in range(len(text)//need_len_to_make_char_from_inf_word):
         # decoded_text += chr(int('0b' + ''.join(reversed(list(text[j*need_len_to_make_char_from_inf_word:(j+1)*need_len_to_make_char_from_inf_word]))), 2))
         decoded_text += chr(int('0b' + ''.join(reversed(list(text[j*need_len_to_make_char_from_inf_word:(j+1)*need_len_to_make_char_from_inf_word]))), 2))
@@ -221,51 +234,25 @@ def make_char_from_inf_words(inf_words, need_len_to_make_char_from_inf_word):
 def get_solution(text, num_of_errors):
     # 1 + x + x^2 + x^5 + x^6 + x^9
     g = [1, 0, 0, 0, 1, 0, 1, 1, 1]
-    # g = [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1]
-    # h = [1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1]
-    # g = [1,0,0,0,1,0,1,1,1]
-    # g = [1, 1, 1, 0, 0, 1, 1, 0, 0, 1]
+
     len_of_inf_word = 7
     need_len_to_make_char_from_inf_word = 12
     # t - количесвто исправляемых ошибок
     t = 2
     # n - длина кодовых слов и синдромов
     n = get_n(g, len_of_inf_word)
-    # print('n: ', n)
-
 
     inf_words = get_inf_words(text, len_of_inf_word, need_len_to_make_char_from_inf_word)
-    # print('inf_words: ', inf_words)
 
     code_words = get_code_words(inf_words, g, n)
-    # print('code_words', code_words)
-
-
-
-    #####################
-    # code_word = [1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
-    # code_word = code_words[0]
-    # code_word_with_one_mistake = make_mistake_in_vector(code_words[0], 0) # code_word_with_one_mistake
-    # code_word_with_one_mistake = [1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0] # code_word_with_one_mistake
-    # code_word_with_two_mistake = [1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0] # code_word_with_two_mistake
-    # code_word_with_two_mistake = [1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0] # code_word_with_two_mistake
 
     code_words_with_mistakes = make_mistake_in_vectors(code_words, num_of_errors)
-    # print('code_words_with_mistakes: ', code_words_with_mistakes)
 
     correct_code_words = correct_mistake_in_code_words(code_words_with_mistakes, g, n, t)
 
     decoded_inf_words = get_inf_words_from_code_words(correct_code_words, g, len_of_inf_word)
-    # print('code_words:', code_words)
-    # print('correct_code_words:', correct_code_words)
-    # print('inf_words:', inf_words)
-    # print('decoded_inf_words:', decoded_inf_words)
 
     decoded_text = make_char_from_inf_words(decoded_inf_words, need_len_to_make_char_from_inf_word)
-    # print(text)
-    # print(decoded_text)
-
-    # print(decoded_text == text)
 
     return {
         'initial_text': text,
